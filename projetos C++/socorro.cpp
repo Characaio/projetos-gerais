f07a77cdf451f75216c54bda5db4ee076e441e48
Characaio <characaioeumesmo@gmail.com>
2025-04-04 23:55:49 -0300

    oiiiii

--- projetos C++/socorro.cpp
+++ projetos C++/socorro.cpp
@@ -4,28 +4,10 @@
 using namespace std;
 
 
-void main()
+int main()
 {
-    int escolha;
-    cout << "escolha um tipo \n" >> "1,2,3 ou 4 \n";
-    cin >> escolha;
-    if (escolha == 1){
-        string nota;
-        cout << "qual foi sua nota? \n ";
-    
-        cin >> nota;
-        if ((nota >= 5) && (nota <= 10))
-        {
-            cout << "aprovado";
-        } else if (nota > 10)
-        {
-            cout << "erro";
-        } else{
-            cout << "reprovado";
-        }
-        
-        
-    }
+    cout << "hello world";
+    return 0;
     
    
 
