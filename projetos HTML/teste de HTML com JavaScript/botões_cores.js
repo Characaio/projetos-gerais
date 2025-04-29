2fe8f66c3ba6f1a1580090250c56036673f16d0a
Characaio <characaioeumesmo@gmail.com>
2025-04-29 10:59:52 -0300

    arrumei a cagada

--- projetos HTML/teste de HTML com JavaScript/botões_cores.js
+++ projetos HTML/teste de HTML com JavaScript/botões_cores.js
@@ -1,31 +1,58 @@
-function esconder(){
-    if (!escondido){
-        diva.classList.remove('entra')
-        escondido = true
-      }
-    diva.classList.add('sair')
+
+
+const colors = [
+  'azul',
+  'amarelo',
+  'preto',
+  'verde',
+  'roxo',
+  'rosa'
+]
+const ColorValues = [
+  '#0A01FF',
+  '#FFF201',
+  '#000000',
+  '#34FF01',
+  '#8901FF',
+  '#ED5ED'
+]
+const dark = [
+  2
+
+]
+let animation = false
+function end(){
+  console.log('hi')
+  
+}
+const socorro = document.getElementById("socorro")
+const root = document.documentElement
+let element = document.getElementById('socorro')
+let computedstyle = window.getComputedStyle(element)
+let backgroundcolor = computedstyle.backgroundColor
+function color(target ){
+  if (!animation){
+    animation = true
+    root.style.setProperty('--TargetColor', ColorValues[target])
+
+    if (ColorValues[target] == 2){
+      console.log('cominggggg')
+      socorro.classList.add('forwards')
+    } 
+    else{
+      console.log('stoppppp')
+      socorro.classList.add('backwards')
+    }
+    socorro.classList.remove('ColorChange')
+    socorro.classList.add('ColorChange')
+
+    setTimeout( function(){
+      socorro.style.setProperty('--MainColor', ColorValues[target]);socorro.classList.remove('forwards');
+      socorro.classList.remove('backwards'); socorro.classList.remove('ColorChange'); animation = false
+    },2000)
+    
+    console.log(backgroundcolor)
+    console.log("target is :", colors[target])
+    
   }
-  function azul() {
-    socorro.style.backgroundColor = "blue"
-    socorro.style.color = "black"
-  }
-  function amarelo() {
-    socorro.style.backgroundColor = "yellow"
-    socorro.style.color = "black"
-  }
-  function preto() {
-    socorro.style.backgroundColor = "black"
-    socorro.style.color = "white"
-  }
-  function verde() {
-    socorro.style.backgroundColor = "green"
-    socorro.style.color = "white"
-  }
-  function roxo() {
-    socorro.style.backgroundColor = "purple"
-    socorro.style.color = "white"
-  }
-  function rosa() {
-    socorro.style.backgroundColor = "pink"
-    socorro.style.color = "black"
-  }
+}
--- projetos HTML/teste de HTML com JavaScript/formulario.js
+++ projetos HTML/teste de HTML com JavaScript/formulario.js
@@ -2,15 +2,16 @@ const login = document.getElementById("login")
 const diva = document.getElementById("diva")
 const opções = [
   `<form id="formulario">
-  <label> Nome: <br> <input class="texto" type="text" name="nome" required></label><br>
+  <label> Nome: <br> <input class="texto" type="text" name="nome" placeholder="SeuNomeAqui" required></label><br>
   <label> 
-  <input type="radio" name = "sexo" value="masculino" required> masculino
-  <input type="radio" name = "sexo" value="feminino" required> feminino
-  <input type="radio" name = "sexo" value="prefiro não dizer" required> prefiro não dizer
+  <input type="radio" name = "sexo" value="masculino" required> Masculino
+  <input type="radio" name = "sexo" value="feminino" required> Feminino
+  <input type="radio" name = "sexo" value="prefiro não dizer" required> Prefiro Não Dizer
   </label><br><br>
-  <label> idade <br> <input type="text" name="idade" required></label><br>
+  <label> Idade <br> <input type="text" name="idade" placeholder="SuaIdade" required></label><br>
   <label> Telefone <br> <input type="text" name="telefone" placeholder="DD xxxxx-xxxx" required></label><br>
-  <label> Email <br> <input type="email" name="email" required></label> <br> <br>
+  <label> Email <br> <input type="email" name="email" placeholder="SeuEmailAqui@gmail.com" required></label> <br><br>
+  <label> Estado Civill
   <select name="EstadoCivil" name="estadocivil" required>
   <option value="selecione">selecione</option>
   <option value="Casada(o)">Casada(o)</option>
@@ -18,17 +19,18 @@ const opções = [
   <option value="Viuva(o)">Viuva(o)</option>
   <option value="Divorcida(o)">Divorcida(o)</option>
   </select> 
-  </form>
+  </label>
+  </form> <br>
   <button id="voltar"> anterior </button>
   <button id="proximo"> proximo</button>
   `,
   `
   <form id="formulario">
   <legend>Endereço</legend>
-  <label>rua <br> <input type="text" name="rua" required></label> <br>
+  <label>Rua <br> <input type="text" name="rua" required></label> <br>
   <label>Número da casa<br> <input type="text" name="numero" required></label> <br>
   <label>Cidade <br> <input type="text" name="cidade" required></label> <br>
-  <label> UF:
+  <label> UF: <br>
   <select name="estados" name="estado" required>
   <option value="selecione">selecione</option>
   <option value="AC">Acre</option>
@@ -36,31 +38,33 @@ const opções = [
   <option value="CE">Ceara</option>
   </select> 
   </label>
-  </form>
+  </form> <br>
   <button id="voltar"> anterior </button>
   <button id="proximo"> proximo</button>
   `,
   `
   <form id="formulario">
-  <label> idiomas:
+  <div style="height:8vh">
+  <label> Idiomas: <br>
   <input type="radio" name="idioma" value="ingles" required> Inglês
   <input type="radio" name="idioma" value="espanhol" required> Espanhol
   <input type="radio" name="idioma" value="frances" required> Francês
   <input type="radio" name="idioma" value="italiano" required> Italiano
   <input type="radio" name="idioma" value="alemao" required> Ilemão
   </label>
-  </form>
+  </div>
+  </form> <br>
   <button id="voltar"> anterior </button>
-  <button id="finalizar"> finalizar </button>
+  <button id="proximo"> proximo</button>
   `,
   `
   <form id="formulario">
-  <label> Login: <br> <input class="texto" type ="text" name="login" value="login placeholder="SeuNome" required> <br><br>
-  <label> Senha: <br> <input class="texto" type ="text" name="senha" value="senha placeholder="SeuNome" required> <br><br>
-  <label> Confirmar Senha: <br> input class="texto" type ="text" name="senha2" value="senha2 placeholder="SeuNome" required> <br><br>
-  </form>
+  <label> Login: <br> <input type ="text" name="login" value="login" placeholder="SeuLogin" required> </label> <br><br>
+  <label> Senha: <br> <input type ="text" name="senha" value="senha" placeholder="Senha" required> </label> <br><br>
+  <label> Confirmar Senha: <br> <input type="texto" type ="text" name="senha2" value="senha2" placeholder="Sua Senha Novamente" required> </label> <br><br>
+  </form> <br>
   <button id="voltar"> anterior </button>
-  <button id="finalizar"> finalizar </button>
+  <button id="proximo"> proximo</button>
   `,
   `
   <form id="formulario">
@@ -71,7 +75,7 @@ const opções = [
   </label><br><br>
   <label> Deixe-nos uma mensagem <br> <textarea name="mensagem" cols="70"  rows="10">  </textarea></label><br><br>
   <label> Senha: <input type="password" name="senha" required></label>
-  </form>
+  </form> <br>
   <button id="voltar"> anterior </button>
   <button id="finalizar"> finalizar </button>
   `
@@ -119,12 +123,12 @@ if (id === "proximo"){
 }
 
 if (id == "voltar"){
-  if (escolha != 0){
+  if (escolha == 0){
     console.log('NãO DA MLK')
   }
   else{
     escolha -= 1
-    diva.innerHTML = opções[escolha]
+    
   }
 
   }
@@ -136,5 +140,6 @@ if (id == "voltar"){
     PushData()
     esconder()
   }
+  diva.innerHTML = opções[escolha]
 }
 )
